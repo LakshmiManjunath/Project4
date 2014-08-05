67833d223c6031c3ccae8dc395872d70e1a3771b
LakshmiManjunath <lakshmi.cs87@gmail.com>
2014-08-04 23:30:57 -0400

    ...

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => 'tunnel.pagodabox.com',
 			'database'  => 'day_book',
-			'username'  => 'root',
-			'password'  => 'root',
+			'username'  => 'teresia',
+			'password'  => '1SIGBbfR',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
